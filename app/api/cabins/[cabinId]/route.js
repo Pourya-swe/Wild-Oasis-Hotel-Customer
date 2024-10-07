7b21bdd29d8814f224450a1aca25edd839c1648a
Pourya-swe <pourya.swe@gmail.com>
2024-10-07 19:21:18 +0330

    Changed data fetching

--- app/api/cabins/[cabinId]/route.js
+++ app/api/cabins/[cabinId]/route.js
@@ -1,20 +1,21 @@
 // Note: Now from this file we can export one or more functions where each of them can correspond to one the Http verbs.
 
-import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
+// import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
 
 // Note: In order to send back response or to check out the request itself, these route handlers use web standards such as "Request" and "Response".
 // Note: We can't give these functions custome names.
-export async function GET(request, { params }) {
-  const { cabinId } = params;
+// export async function GET(request, { params }) {
+//   const { cabinId } = params;
 
-  try {
-    const [cabin, bookedDates] = await Promise.all([
-      getCabin(cabinId),
-      getBookedDatesByCabinId(cabinId),
-    ]);
-    return Response.json({ cabin, bookedDates });
-  } catch {
-    return Response.json({ message: "Cabin not found!" });
-  }
-}
+//   try {
+//     const [cabin, bookedDates] = await Promise.all([
+//       getCabin(cabinId),
+//       getBookedDatesByCabinId(cabinId),
+//     ]);
+//     return Response.json({ cabin, bookedDates });
+//   } catch {
+//     return Response.json({ message: "Cabin not found!" });
+//   }
+// }
+
 // export async function POST() {}
